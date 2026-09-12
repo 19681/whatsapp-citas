29d82e42aaa5f2f3aae5296cc6713c0528d0543b
19681 <80593990+19681@users.noreply.github.com>
2026-09-12 13:41:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const __dirname = path.dirname(__filename);
 import baileys from '@whiskeysockets/baileys';
 const makeWASocket = baileys.default || baileys;
 const { useMultiFileAuthState, DisconnectReason } = baileys;
+const makeWASocket = baileys.default || baileys;
+const { useMultiFileAuthState, DisconnectReason } = baileys;
 import makeWASocket, { useMultiFileAuthState, DisconnectReason } from '@whiskeysockets/baileys';
 import express from 'express';
 import cron from 'node-cron';
